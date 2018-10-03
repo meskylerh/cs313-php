1fd87ae48f08a5e49b13b12f72bc7cc5f56b6ce0
meskylerh <ahxinfiltrator@yahoo.com>
2018-10-03 15:47:35 -0600

    fix

--- web/teach/display.php
+++ web/teach/display.php
@@ -2,7 +2,7 @@
 <body>
 <?php
 echo "username:" . "<br>" .   $_POST["Name"] . "<br><br>";
-echo "Email:" . "<br>" .   $_POST["Email"] . "<br><br>";
+echo "Email:" . "<br>" .<"a href-'mailto'" .  $_POST["Email"] . "<a><br><br>";
 echo "Major:" . "<br>" . $_POST["Major"] . "<br><br>";
 echo "Comments:" . "<br>"  . $_POST["comments"] . "<br><br>";
 
