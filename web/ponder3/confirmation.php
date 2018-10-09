f686275d2c8dbb124bd36b381a55d153638afc04
meskylerh <ahxinfiltrator@yahoo.com>
2018-10-08 22:09:14 -0600

    not the last time i will commit im sure:)

--- web/ponder3/confirmation.php
+++ web/ponder3/confirmation.php
@@ -5,7 +5,7 @@
 <body>
 <h1>Thank you</h1>
 <?php
-echo $_POST["Name"] . "<br><br>";
+echo $_POST["name"] . "<br><br>";
 echo "Your order will be sent to:" . "<br><br>";
 echo $_POST["street"] . " " . $_POST["city"] . "<br>" . $_POST["state"]. "  " . $_POST["zip"]. "<br><br>";
 
