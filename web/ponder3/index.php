90fa41f86b7feea6c8e3cf75750de935041dad89
meskylerh <ahxinfiltrator@yahoo.com>
2018-10-08 22:12:14 -0600

    not the last time i will commit im sure:)

--- web/ponder3/index.php
+++ web/ponder3/index.php
@@ -26,7 +26,7 @@
    This is the first release in the Birds of Prey series from the Royal Canadian Mint.<br>
    The Birds of Prey series is the successor of the popular Canadian Wildlife Series<br><br>
    <label>Price</label><br>
-   $19.47
+   $19.47<br>
    <input type="checkbox" name="silver[]" value="Peregrine 1oz" ><label>ADD TO CART!!!<label><br><br>
    
    
@@ -36,7 +36,7 @@
 	The Royal Canadian Mint has long been known for high quality .9999 fine Silver <br>
    products and this uniquely sized item continues that tradition.<br><br>
    <label>Price</label><br>
-   $21.09
+   $21.09<br>
    <input type="checkbox" name="silver[]" value="Bison 1.25oz" ><label>ADD TO CART!!!<label><br><br>
    
    <img src="s3.jpg" style="width:200px;height:200px;"><br>
@@ -45,7 +45,7 @@
    Sold out at the mint. 2016 is the 30th anniversary of the 1 oz American Silver Eagle bullion<br>
    coin program from the United States Mint.<br><br>
    <label>Price</label><br>
-   $19.47
+   $19.47<br>
    <input type="checkbox" name="silver[]" value="Eagle 1oz" ><label>ADD TO CART!!!<label><br><br>
    
    <img src="s4.png" style="width:200px;height:200px;"><br>
@@ -54,7 +54,7 @@
    Depicts Thor wielding his hammer, wearing his belt and gripping the fang of the serpent Jormungand.<br>
    Also included in the design is a goat’s head, lightning and the inscription "THOR"..<br><br>
    <label>Price</label><br>
-   $124.52
+   $124.52<br>
    <input type="checkbox" name="silver[]" value="Thor 2oz" ><label>ADD TO CART!!!<label><br><br>
    
    <img src="s5.jpg" style="width:200px;height:200px;"><br>
@@ -62,7 +62,7 @@
   <label> Description:</label><br>
    This release in the Predator Series from the Royal Canadian Mint (RCM) features the fearsome predator known as the cougar.<br><br>
    <label>Price</label><br>
-   $20.97
+   $20.97<br>
    <input type="checkbox" name="silver[]" value="Thor 2oz" ><label>ADD TO CART!!!<label><br><br>
    
    <input type="submit" value="Add to cart">
