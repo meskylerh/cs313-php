bb6c285155a37e2ca5a2413cdc6f67c1614208cf
meskylerh <ahxinfiltrator@yahoo.com>
2018-10-08 22:05:15 -0600

    not the last time i will commit im sure:)

--- web/ponder3/confirmation.php
+++ web/ponder3/confirmation.php
@@ -1,18 +1,18 @@
 <html>
 <body>
-<?php\
-echo "Thank you";
+<h1>Thank you</h1>
+<?php
 echo "Name:" . "<br>" .   $_POST["Name"] . "<br><br>";
 echo "Your order will be sent to:";
 echo $_POST["street"] . " " . $_POST["city"] . "<br>" . $_POST["state"]. "  " . $_POST["zip"];
 
-echo "Your order:"
+echo "Your order:";
 
 				$silver = $_SESSION["cart"];
 				foreach($silver as $silvera){
-				echo "<strong>" .$silvera."</Strong>"."<br />";
+				echo "<strong>" .$silvera."</Strong>"."<br />";}
 				
 echo "enjoy the rest of your day!";
 				?>
-				</body>
-				</html>
+</body>
+</html>
