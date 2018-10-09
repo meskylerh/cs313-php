eb879ebd101f7322b281ac97b9d72a9b09b1d636
meskylerh <ahxinfiltrator@yahoo.com>
2018-10-08 21:14:07 -0600

    added index

--- web/ponder3/index.php
+++ web/ponder3/index.php
@@ -17,6 +17,54 @@
                 <main>
                     <h2>Welcome</h2>
 					<?php include ("nav.php"); ?>
+		
+<form action="addToCart.php" method="post">
+	<img src="s1.jpg" style="width:200px;height:200px;">
+   <label> Silver Peregrine 1oz .9999 silver</label><br>
+   <label> Descirption</label><br>
+   This is the first release in the Birds of Prey series from the Royal Canadian Mint.<br>
+   The Birds of Prey series is the successor of the popular Canadian Wildlife Series<br><br>
+   <label>Price</label>
+   $19.47
+   <input type="checkbox" name="silver[]" value="Peregrine 1oz" ><label>ADD TO CART!!!<label><br><br>
+   
+   <img src="s2.jpg" style="width:200px;height:200px;">
+   <label> Silver Bison 1.25oz .9999 silver</label><br>
+   <label> Descirption</label><br>
+	The Royal Canadian Mint has long been known for high quality .9999 fine Silver <br>
+   products and this uniquely sized item continues that tradition.<br><br>
+   <label>Price</label>
+   $21.09
+   <input type="checkbox" name="silver[]" value="Bison 1.25oz" ><label>ADD TO CART!!!<label><br><br>
+   
+   <img src="s3.jpg" style="width:200px;height:200px;">
+   <label> Silver Eagle 1oz .999 silver</label><br>
+   <label> Descirption</label><br>
+   Sold out at the mint. 2016 is the 30th anniversary of the 1 oz American Silver Eagle bullion<br>
+   coin program from the United States Mint.<br><br>
+   <label>Price</label>
+   $19.47
+   <input type="checkbox" name="silver[]" value="eagle 1oz" ><label>ADD TO CART!!!<label><br><br>
+   
+   <img src="s4.png" style="width:200px;height:200px;">
+   <label> Silver norse gods Thor 2oz .999 silver</label><br>
+   <label> Descirption</label><br>
+   Depicts Thor wielding his hammer, wearing his belt and gripping the fang of the serpent Jormungand.<br>
+   Also included in the design is a goat’s head, lightning and the inscription "THOR"..<br><br>
+   <label>Price</label>
+   $124.52
+   <input type="checkbox" name="silver[]" value="Thor 2oz" ><label>ADD TO CART!!!<label><br><br>
+   
+   <img src="s5.jpg" style="width:200px;height:200px;">
+   <label> Silver cougar 1oz .999 silver</label><br>
+   <label> Descirption</label><br>
+   This release in the Predator Series from the Royal Canadian Mint (RCM) features the fearsome predator known as the cougar.<br><br>
+   <label>Price</label>
+   $20.97
+   <input type="checkbox" name="silver[]" value="Thor 2oz" ><label>ADD TO CART!!!<label><br><br>
+   
+   <input type="submit" value="Add to cart">
+</form>
 		</div>
 		      <footer>
             <p>created By Skyler Hildreth enjoy</p>
