422f7f25dff600138e4159cf7da49696e8f40128
meskylerh <ahxinfiltrator@yahoo.com>
2018-10-11 23:53:20 -0600

    sql

---
+++ db/myDb.sql
@@ -0,0 +1,33 @@
+CREATE TABLE public.user
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	username VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE public.metal
+(
+	id INT NOT NULL PRIMARY KEY,
+	btype bullion NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	description TEXT NOT NULL,
+	price DECIMAL NOT NULL,
+	type metalType NOT NULL
+);
+
+Create Table public.collection
+(
+   id INT NOT NULL PRIMARY KEY,
+   collection_name VARCHAR(100) not null,
+   user_id INT references public.user(id),
+   metal_id INT references public.metal(id),
+   quantity INT NOT NULL
+);
+
+
+create type metalType as enum ('silver', 'gold', 'Platinum', 'copper');
+create type bullion as enum ('coin', 'bar', 'round');
+
+
+alter table public.metal
+add column weight DECIMAL NOT NULL;
