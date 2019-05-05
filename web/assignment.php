517dbcc6312b43e0ef2f7692ab803c234a0c9ecf
meskylerh <ahxinfiltrator@yahoo.com>
2019-05-04 18:25:18 -0600

    changes

--- web/assignment.php
+++ web/assignment.php
@@ -6,7 +6,8 @@
     <title>Skyler's Assignment page</title>
     <meta name="description" content="contents of page">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="Stylei.css" />
+   <link href="https://fonts.googleapis.com/css?family=Wendy+One" rel="stylesheet">
 </head>
     <body>
         <div class="container">
@@ -15,14 +16,14 @@
         </header>
             <div class="flex">
                 <main>
-                    <h2>Welcome</h2>
+                    
 					<?php include("nav.php"); ?>
+               <h2>Welcome</h2>
 		</div>
                   <p>"Comming Soon"</p>  
                   
                 </main>
            
-            <?php include ("footer.php"); ?>
         </div>
     </body>
 
