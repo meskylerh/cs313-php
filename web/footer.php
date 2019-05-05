72c18a3059a8e8c81e59307d16b220f2068857a5
meskylerh <ahxinfiltrator@yahoo.com>
2019-05-04 18:15:54 -0600

    changes

--- web/footer.php
+++ web/footer.php
@@ -1,7 +1,7 @@
 		<div class="footer">
+       <p>created By Skyler Hildreth enjoy</p>
 			<ul>
 				<li class="home"><a href="intro.php">Home</a></li>
 				<li class="assignments"><a href="assignment.php"> assignments </a></li>
-   
 			</ul>
-         <p>created By Skyler Hildreth enjoy</p>
+        
