67b7e81650ea13c448d22c901bdbd9ebdc66b108
meskylerh <ahxinfiltrator@yahoo.com>
2019-05-07 21:53:25 -0600

    added changes

--- web/teach/03.php
+++ web/teach/03.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Teach 03</title>
+    
     <meta name="description" content="contents of page">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
