b06d7e21062b6adc34a12539cc6d9d8e8aea18db
meskylerh <ahxinfiltrator@yahoo.com>
2019-05-07 22:35:43 -0600

    added changes

--- web/teach/display.php
+++ web/teach/display.php
@@ -13,9 +13,10 @@ echo "Comments:" . "<br>"  . $_POST["comments"] . "<br><br>";
 echo "Continents Vistited:" . "<br>";
     if(isset($_POST['abrev'])) {
       $continents = $_POST['abrev'];  
-      $States = array("NA"=>"North America", "SA"=>"South America", "EU"=>"Europe", "AS" =>"Asia", "AU"=>"Australia", "AF"=>"Africa", "AN"=>"Antartica"); 
+      $states = array("NA"=>"North America", "SA"=>"South America", "EU"=>"Europe", "AS" =>"Asia", "AU"=>"Australia", "AF"=>"Africa", "AN"=>"Antartica"); 
     foreach ($continents as $contValue){
-   echo $States[$continents]"<br />";
+       $fullstate = $states[$continents]
+   echo $fullstate "<br />";
   }}
   
   
