150b38291bc77e188a9fa17f6efc3a92d9ef6d05
meskylerh <ahxinfiltrator@yahoo.com>
2019-06-08 16:13:16 -0600

    Update myDb.sql

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,52 +1,41 @@
-CREATE TABLE public.user
+CREATE TABLE users
+
 (
-	id SERIAL NOT NULL PRIMARY KEY,
+	user_id SERIAL NOT NULL PRIMARY KEY,
 	username VARCHAR(100) NOT NULL UNIQUE,
-	password VARCHAR(100) NOT NULL
+	 password VARCHAR(100) NOT NULL
+
 );
 
-CREATE TABLE public.band
-(
-	band_id INT NOT NULL PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
-	type genre  NOT NULL,
-);
 
-Create Table public.showtime
-(
-   showtime_id INT NOT NULL PRIMARY KEY,
-   Date VARCHAR(100) not null,
-   time TIME Not null
-);
+CREATE TABLE menu
+(	menu_id SERIAL NOT NULL PRIMARY KEY,
 
-CREATE TABLE public.food
-(
-	food_id INT NOT NULL PRIMARY KEY,
 	name Varchar(100) Not Null,
-	price DECIMAL Not Null
+
+	price DECIMAL Not Null,
+	day DAY NOT NULL
 );
-Create TABLE public.menu
-(	
-	menu_id INT NOT NULL PRIMARY KEY,
-	meal meal NOT NULL,
-	name VARCHAR(100) references public.food(name),
-	price DECIMAL references public.food(price)
-);
-	
-CREATE TABLE public.ticket
+
+
+CREATE TABLE event
+
 (
-	ticket_id INT NOT NULL PRIMARY KEY,
-	date Varchar(100) references Public.showtime(Date),
-	time TIME references public.showtime(time),
-	name VARCHAR(100) references public.band(name)
-	price DECIMAL NOT NULL
+	event_id SERIAL NOT NULL PRIMARY KEY,
+
+	name VARCHAR(100) NOT NULL,
+	date DATE NOT NULL,
+
+	time TIME NOT NULL,
+	type genre  NOT NULL
+
+,	accepted BOOLEAN NOT NULL
 );
-	
-	
 
 
-Create type meal as enum ('breakfast', 'lunch', 'dinner');
+
+	
 create type genre as enum ('country', 'metal', 'rock', 'disco', 'pop');
-
+create type DAY as enum ('Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday');
 
 
