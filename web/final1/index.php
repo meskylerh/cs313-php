009f345365925c1877caeadc15773098e4e3d6e6
meskylerh <ahxinfiltrator@yahoo.com>
2019-06-08 22:30:49 -0600

    adding project1

--- web/final1/index.php
+++ web/final1/index.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; 
 $db = getDb();
-$statement = $db->prepare("SELECT name, type, date, time FROM event WHERE accepted = TRUE");
+$statement = $db->prepare("SELECT name, type, date, time FROM event WHERE accepted = TRUE ORDER BY date DESC");
 $statement->execute();
 
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
