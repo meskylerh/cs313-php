eb2e3faa0b1321e70820eb0beffeb670cbd5e49a
meskylerh <ahxinfiltrator@yahoo.com>
2019-06-09 16:44:03 -0600

    style

--- web/final1/submit_request.php
+++ web/final1/submit_request.php
@@ -7,7 +7,9 @@ $time = $_POST['show-time'];
 
 $sql = "INSERT INTO event (name, date, time, type, accepted)values('$bandname', '$date', '$time', '$genre',FALSE)";
 $db->prepare($sql)->execute(); 
-echo '<h1>Thank You For Submitting a Request to play at the Rusty Blue</h1>';
+echo '<h1>Thank you for submitting a request to play at the Rusty Blue!</h1>
+      <p>We will review your request shortly</p>'
+;
 
 
  include 'footer.php'; ?>
