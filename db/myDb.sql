92372a409003be4d226aa542c6728147f744f644
meskylerh <ahxinfiltrator@yahoo.com>
2019-06-09 17:21:24 -0600

    fix database spacing

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,40 +1,29 @@
 CREATE TABLE users
-
 (
 	user_id SERIAL NOT NULL PRIMARY KEY,
 	username VARCHAR(100) NOT NULL UNIQUE,
-	 password VARCHAR(100) NOT NULL
-
+	password VARCHAR(100) NOT NULL
 );
 
 
 CREATE TABLE menu
 (	menu_id SERIAL NOT NULL PRIMARY KEY,
-
 	name Varchar(100) Not Null,
-
 	price DECIMAL Not Null,
 	day DAY NOT NULL
 );
 
 
 CREATE TABLE event
-
 (
 	event_id SERIAL NOT NULL PRIMARY KEY,
-
 	name VARCHAR(100) NOT NULL,
 	date DATE NOT NULL,
-
 	time TIME NOT NULL,
-	type genre  NOT NULL
-
-,	accepted BOOLEAN NOT NULL
+	type genre  NOT NULL,
+	accepted BOOLEAN NOT NULL
 );
 
-
-
-	
 create type genre as enum ('country', 'metal', 'rock', 'disco', 'pop');
 create type DAY as enum ('Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday');
 
