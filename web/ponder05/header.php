ed9337464bfad04a3de70f3fdfe9e84fc2f34222
meskylerh <ahxinfiltrator@yahoo.com>
2019-06-13 20:30:59 -0600

    database acess

---
+++ web/ponder05/header.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+require 'db.php';
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <link rel="stylesheet" type="text/css" href="Style.css" />
+      <Title>Rusty Blue</Title>
+   </head>
+<body>
+       <div class="nav">
+         <ul>
+         <li><a href="index.php" class="button-class">Home</a></li>
+         <li><a href="menu.php" class="button-class">Menu</a></li>
+         <li><a href="request.php" class="button-class">Request to Play</a></li>
+         <!--only to view the admin page will not be here if not needed to be seen-->
+         <li><a href="admin.php" class="button-class">Admin</a></li>
+         </ul>
+       </div>
+       <div class="container">
